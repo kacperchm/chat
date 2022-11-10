c4c4bdbd88f86243eb0a3be2174cc239a359f379
kacperchm <chmuraka1998@gmail.com>
2022-11-11 00:00:59 +0100

    Initial commit

---
+++ src/main/java/com/github/kacperchm/chat/ChatMessageController.java
@@ -0,0 +1,16 @@
+package com.github.kacperchm.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatMessageController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+
+    public ChatMessage get(ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
